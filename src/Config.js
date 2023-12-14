4af3e116ef99541b04c618cbb81f7eb458b8bcfc
namratha-p-s <namratha.psom.97@gmail.com>
2023-12-14 22:48:53 +0530

    Reverted proxyURL

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,3 @@
-const proxyUrl = process.env.ZEPHYR_PROXY_URL || 'http://zephyr-2.ddns.net:5001';
+const proxyUrl = process.env.ZEPHYR_PROXY_URL || 'http://localhost:5001';
 
 export default proxyUrl;
