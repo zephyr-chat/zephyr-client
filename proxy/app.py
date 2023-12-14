3dd8e310c447c4d90dff98505485472238f68ac8
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-12-14 02:20:42 -0700

    Add get conversation to flask

--- proxy/app.py
+++ proxy/app.py
@@ -1,10 +1,15 @@
+import json
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from grpc import insecure_channel
+from google.protobuf.json_format import MessageToJson
 
 from config import *
 import auth_pb2_grpc
 import auth_pb2
+import conversation_pb2_grpc as conv_pb2_grpc
+import conversation_pb2 as conv_pb2
 
 
 app = Flask(__name__)
@@ -37,6 +42,37 @@ def authenticate():
         print(f"Error occured while authentication {str(e)}")
         response = jsonify({'message':'Error while authenticating with server'})
         return response, 401
+
+def get_conversation(request):
+    headers = request.headers
+    access_token = headers.get('Authorization', None)
+
+    if not access_token:
+        print(f"Access token not found")
+        response = jsonify({'message':'No access token, unauthorized'})
+        return response, 401
+    
+    try:
+        stub = conv_pb2_grpc.ConversationServiceStub(channel)
+        req = conv_pb2.Empty()
+        metadata = [(b'authorization', access_token.encode())]
+        reply: auth_pb2.AuthResponse = stub.GetConversations(req, metadata=metadata)
+        return jsonify(json.loads(MessageToJson(reply))), 200 
+    except Exception as e:
+        print(f"Error occured while authentication {str(e)}")
+        response = jsonify({'message':'Error while authenticating with server'})
+        return response, 401
+
+def create_conversation(request):
+    response = jsonify({'message':'Not implemented'})
+    return response, 401
+
+@app.route('/conversation', methods=['GET', 'POST'])
+def conversations():
+    if request.method == 'GET':
+        return get_conversation(request)
+    elif request.method == 'POST':
+        return create_conversation(request)
 
 
 if __name__ == '__main__':
