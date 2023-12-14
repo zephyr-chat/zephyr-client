3249f9c65559b036d03526bc64e0f7feedd65fed
Akshit Panapuzha <akshitpanapuzha@Akshits-MacBook-Pro.local>
2023-12-14 12:33:00 -0700

    register form

---
+++ src/RegisterForm.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // API call to register the user
+    fetch('http://localhost:5001/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        displayname: displayName,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Registration response:', data); // Log the response from the API
+        // Handle response as needed (e.g., update state or show a success message)
+      })
+      .catch((error) => {
+        console.error('Error registering user:', error);
+        // Handle error scenario
+      });
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Display Name:</label>
+          <input
+            type="text"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
